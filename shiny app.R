1c509b29e93b4ceaa73b57460076a2b8106688f3
luigi.aceto <luigi.aceto@genre.com>
2023-07-08 08:27:06 +0200

    change added more parameters to reactable fun

--- shiny app.R
+++ shiny app.R
@@ -8,9 +8,28 @@ ui <- fluidPage(
 
 server <- function(input, output, session) {
   output$table <- renderReactable({
-    reactable(iris)
+    reactable(
+      iris,
+      defaultColDef = colDef(
+        header = function(value) gsub(".", " ", value, fixed = TRUE),
+        cell = function(value) format(value, nsmall = 1),
+        align = "center",
+        minWidth = 70,
+        headerStyle = list(background = "#f7f7f8")
+      ),
+      columns = list(
+        Species = colDef(minWidth = 140),  # overrides the default,
+        Sepal.Length = colDef(filterable = FALSE),
+        Sepal.Width = colDef(filterable = FALSE),
+        Petal.Length = colDef(filterable = FALSE),
+        Petal.Width = colDef(filterable = FALSE)
+      ),
+      bordered = TRUE,
+      highlight = TRUE, 
+      filterable = TRUE,
+      showPagination = TRUE
+    )
   })
 }
 
 shinyApp(ui, server)
-}
